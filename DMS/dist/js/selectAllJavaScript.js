1593c884276bb10d8ad28af90e97da815ee72e80
nkpolina <nateshk@metricstream.com>
2015-04-24 16:47:18 +0530

    DMS Commit
    DMS Commit

---
+++ DMS/dist/js/selectAllJavaScript.js
@@ -0,0 +1,20 @@
+function SelectText(element) {
+    var doc = document
+        , text = doc.getElementById(element)
+        , range, selection
+    ;    
+    if (doc.body.createTextRange) {
+        range = document.body.createTextRange();
+        range.moveToElementText(text);
+        range.select();
+        range.execCommand("copy",true);
+    } else if (window.getSelection) {
+        selection = window.getSelection();        
+        range = document.createRange();
+        range.selectNodeContents(text);
+        selection.removeAllRanges();
+        selection.addRange(range);
+        range.execCommand("copy",true);
+    }
+     document.execCommand("copy",true);
+}
