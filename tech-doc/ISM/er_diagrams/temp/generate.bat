d7600549337634a356d72b11877cb115f740f1a0
Gopalakrishnan P <gpuliyar@metricstream.com>
2017-03-17 13:26:52 +0000

    commit

---
+++ tech-doc/ISM/er_diagrams/temp/generate.bat
@@ -0,0 +1,7 @@
+cd/
+cd C:/dev/report//ISM_7.0.0.0.0.0.0.20170222125113/ISM_7.0.0.0.0.0.0.20170222125113/er_diagrams/temp
+call C:\dev\devtools\graphviz-2.38\release\bin/dot -Kfdp -Tpdf C:/dev/report//ISM_7.0.0.0.0.0.0.20170222125113/ISM_7.0.0.0.0.0.0.20170222125113/er_diagrams/temp/source_dot\MSISMACTION.dot -o C:/dev/report//ISM_7.0.0.0.0.0.0.20170222125113/ISM_7.0.0.0.0.0.0.20170222125113/er_diagrams/erd_images\MSISMACTION.pdf
+call C:\dev\devtools\graphviz-2.38\release\bin/dot -Kfdp -Tpdf C:/dev/report//ISM_7.0.0.0.0.0.0.20170222125113/ISM_7.0.0.0.0.0.0.20170222125113/er_diagrams/temp/source_dot\MSISMISSUE.dot -o C:/dev/report//ISM_7.0.0.0.0.0.0.20170222125113/ISM_7.0.0.0.0.0.0.20170222125113/er_diagrams/erd_images\MSISMISSUE.pdf
+call C:\dev\devtools\graphviz-2.38\release\bin/dot -Kfdp -Tpdf C:/dev/report//ISM_7.0.0.0.0.0.0.20170222125113/ISM_7.0.0.0.0.0.0.20170222125113/er_diagrams/temp/source_dot\ISM.dot -o C:/dev/report//ISM_7.0.0.0.0.0.0.20170222125113/ISM_7.0.0.0.0.0.0.20170222125113/er_diagrams/erd_images\ISM.pdf
+
+exit
